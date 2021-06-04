41471e67c92afe6c8dd7b12dd47ec93b32c0c26d
Sreenivasulu Gundagula <sreenivasulu.gundagula@ibm.com>
2021-06-04 11:09:26 +0530

    update1

---
+++ exchange.java
@@ -0,0 +1,18 @@
+public class exchange
+{
+  public static void main(String[] args) 
+  {
+
+    int num = 1728, reversed = 0;
+
+    while(num != 0) 
+	{
+    
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+      num /= 10;
+    }
+
+    System.out.println("Reversed Number: " + reversed);
+  }
+}
